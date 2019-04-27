6767625d7e1a464b6cdb7344579c32d20b1ae53a
Javohir Avlikhodjaev <deer.friend.4ever@gmail.com>
2019-04-27 12:57:37 +0500

    IntegrationEventLogEF Boilerplate

---
+++ src/BuildingBlocks/IntegrationEventLogEF/EventStateEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace Athena.BuildingBlocks.IntegrationEventLogEF
+{
+    public enum EventStateEnum
+    {
+        NotPublished = 0,
+        InProgress = 1,
+        Published = 2,
+        PublishedFailed = 3
+    }
+}
---
+++ src/BuildingBlocks/IntegrationEventLogEF/Services/IIntegrationEventLogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Athena.BuildingBlocks.IntegrationEventLogEF.Services
+{
+    public interface IIntegrationEventLogService
+    {
+        Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync();
+        Task SaveEventAsync(IntegrationEvent @event, DbTransaction transaction);
+        Task MarkEventAsPublishedAsync(Guid eventId);
+        Task MarkEventAsInProgressAsync(Guid eventId);
+        Task MarkEventAsFailedAsync(Guid eventId);
+    }
+}
