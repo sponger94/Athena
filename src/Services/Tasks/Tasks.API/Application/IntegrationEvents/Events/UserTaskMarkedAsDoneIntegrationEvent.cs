d60e13cc7b336e576d95b303f14083ff5a48ea98
Javohir Avlikhodjaev <deer.friend.4ever@gmail.com>
2019-04-30 07:54:10 +0500

    UserTask done/undone integration events.

---
+++ src/Services/Tasks/Tasks.API/Application/IntegrationEvents/Events/UserTaskMarkedAsDoneIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using Athena.BuildingBlocks.EventBus.Events;
+
+namespace Tasks.API.Application.IntegrationEvents.Events
+{
+    public class UserTaskMarkedAsDoneIntegrationEvent : IntegrationEvent
+    {
+        public int UserTaskId { get; }
+        public string UserTaskName { get; }
+
+        public UserTaskMarkedAsDoneIntegrationEvent(int userTaskId, string userTaskName)
+        {
+            UserTaskId = userTaskId;
+            UserTaskName = userTaskName;
+        }
+    }
+}
---
+++ src/Services/Tasks/Tasks.API/Application/IntegrationEvents/Events/UserTaskMarkedAsUnDoneIntegrationEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Athena.BuildingBlocks.EventBus.Events;
+
+namespace Tasks.API.Application.IntegrationEvents.Events
+{
+    public class UserTaskMarkedAsUnDoneIntegrationEvent : IntegrationEvent
+    {
+        public int UserTaskId { get; }
+        public string UserTaskName { get; }
+
+        public UserTaskMarkedAsUnDoneIntegrationEvent(int userTaskId, string userTaskName)
+        {
+            UserTaskId = userTaskId;
+            UserTaskName = userTaskName;
+        }
+    }
+}
